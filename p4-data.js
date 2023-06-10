ff6892782701dc66a03afd2877c6c2566348da3d
keitabacon <100260920+keitabacon@users.noreply.github.com>
2023-06-09 21:30:37 -0700

    Create p4-data.js

---
+++ p4-data.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    question: "Q1",
+    answer: "A1",
+  },
+  {
+    question: "Q2",
+    answer: "A2",
+  },
+  {
+    question: "Q3",
+    answer: "A3",
+  },
+];
+
+module.exports = data;
